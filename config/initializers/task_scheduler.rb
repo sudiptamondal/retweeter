0d70fa86f5cff75b52ac25532c26463515a1dac6
Cloud <cloud@cloud>
2013-01-02 22:46:31 +0530

    removed stupid puts statements

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -2,6 +2,5 @@ require 'rubygems'
 require 'rufus/scheduler'
 scheduler = Rufus::Scheduler.start_new
 scheduler.every("1m") do
-	puts "Checking for new tweets @" + Time.now.to_s
   Tweet.scheduled_check
 end
