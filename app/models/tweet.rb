f7d044352a42d8a157931caa266af6597e87ae2d
Cloud <cloud@cloud>
2013-01-02 23:43:32 +0530

    cleaned up messages

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,35 +3,17 @@ class Tweet < ActiveRecord::Base
 
   def self.scheduled_check
     begin
-
       latest_tweets = Twitter.user_timeline("delhianimeclub")
- 
       latest_tweets.each do |tweet|
-        puts "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
-        puts "current_tweet_id:       ::::   " + tweet.attrs[:id].to_s
-        puts "current_tweet_created_at::::   " + tweet.attrs[:created_at].to_s
-        puts "current_tweet_text      ::::   " + tweet.attrs[:text].to_s
-        puts "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
-        
         tweet_present_in_db = false
-
         Tweet.all.each do |db_tweet|
-          if db_tweet.tweet_id == tweet.attrs[:id].to_s
-            tweet_present_in_db = true
-          end
+          tweet_present_in_db = true if db_tweet.tweet_id == tweet.attrs[:id_str].to_s
         end
-        
         if !tweet_present_in_db
-          puts "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
-          puts "Adding a new tweet"
-          Tweet.create(:tweet_id=>tweet.attrs[:id_str], :tweet_text=>tweet.attrs[:text], :tweeted_at => tweet.attrs[:created_at])
-          puts "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
           tweet_text = ("RT @delhianimeclub " + tweet.attrs[:text]).slice(0..139)
           Twitter.update(tweet_text)
+          Tweet.create(:tweet_id=>tweet.attrs[:id_str], :tweet_text=>tweet.attrs[:text], :tweeted_at => tweet.attrs[:created_at])
         else
-          puts "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
-          puts "Tweet already present"
-          puts "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
         end
       end
     rescue => e
--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'rufus/scheduler'
 scheduler = Rufus::Scheduler.start_new
+# => The scheduler calls this method evey minute, you can set it to whatever interval you like
 scheduler.every("1m") do
   Tweet.scheduled_check
 end
